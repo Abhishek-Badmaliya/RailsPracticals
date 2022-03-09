b3f861d9a191a64c9068743faad7c6966a9b2223
Abhishek Badmaliya <abhishek.badmaliya@simformsolutions.com>
2022-03-09 15:09:53 +0530

    feat: TE5:T86, active record basics update the changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,10 +15,10 @@ class ProductsController < ApplicationController
     @product=Product.new(product_params)
     if@product.save
       flash[:notice]="Product Added Successfully!"
-      redirct_to_root_path
+      redirect_to products_path
     else
       flash[:error]="Product is not added!"
-      render:new
+      render :new
     end
   end
 
@@ -28,27 +28,27 @@ class ProductsController < ApplicationController
 
   def update
     @product=Product.find(params[:id])
-    if@product.update_attributes(product_params)
+    if@product.update(product_params)
       flash[:notice]="Product Updated Suceessfully!"
-      redirct_to_root_path
+      redirect_to products_path
     else
-      flash[;error]="Updation Operation Failed!"
-      render:edit
+      flash[:error]="Updation Operation Failed!"
+      render :edit
     end
   end
 
   def destroy
     @product=Product.find(params[:id])
-    if@product.delete
+    if@product.destroy
       flash[:notice]="Product is Deleted!"
-      redirct_to_root_path
+      redirect_to products_path
     else
-      flash[;error]="Deletion Operation Failed!" 
-      render:edit
+      flash[:error]="Deletion Operation Failed!"
     end
   end
 
+  private
   def product_params
-    params.require(:product).permit(:name, :price, ;old_price, :short_description, :full_description)
+    params.require(:product).permit(:name, :price, :old_price, :short_description, :full_description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
   get '/users', to: "users#index"
 
-  resources:products
-  root'products#index'
+  resources :products
 end
