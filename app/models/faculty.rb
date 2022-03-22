b5bc181f374a28bef1a8c259aa35fb0ad5b6be17
Abhishek Badmaliya <abhishek.badmaliya@simformsolutions.com>
2022-03-22 19:08:59 +0530

    feat: TE5-T90, create callbacks for student and faculty model

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -10,10 +10,30 @@ class Faculty < ApplicationRecord
 
   validates:first_name, :last_name, :fac_dob, :contact, presence:true
   validates:contact,numericality: { only_integer:true }, length:{is:10}, uniqueness:{case_sensitive:false}
- 
-  VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- 
   validates:email, presence:true, uniqueness:{case_sensitive:false}
   validates:desig,:inclusion => {:in => %w(Ass.Prof Prof),:message => "can't be %{value}" }, :exclusion => { :in => %w(HOD Sr.Prof.),:message => "%{value} is reserved." }
 
+  #it's called whenever created new or load the record
+  after_initialize do |faculty|
+    puts "An object initialized !"
+  end
+
+  #it's called whenever database record is loaded
+  after_find do |faculty|
+    puts "An object found !"
+  end
+
+  #it's called whenever object is touched
+  after_touch do |faculty|
+    puts "An object touched !"
+  end
+  
+  before_validation :check_for_login
+
+  private
+    def check_for_login
+      if login.nil?
+        self.login = email unless email.blank?
+      end
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -11,5 +11,37 @@ class Student < ApplicationRecord
   validates :first_name, :last_name, :stu_dob, presence: true
   validates :terms_of_usage, acceptance: { message: ': You cannot proceed without accepting Terms of Usage' }
   validates :dept, :inclusion => { :in => %w(IT CS), :message => "can't be %{value}" }
-  
+
+  #it's called whenever created new or load the record
+  after_initialize do |student|
+    puts "An object initialized !"
+  end
+
+  #it's called whenever database record is loaded
+  after_find do |student|
+    puts "An object found !"
+  end
+
+  #it's called whenever object is touched
+  after_touch do |student|
+    puts "An object touched !"
+  end
+
+  before_validation :remove_whitespaces
+  def remove_whitespaces
+    first_name.strip!
+  end
+
+  before_save do |student|
+    puts "An object is not saved yet!"
+  end
+
+  after_save do |student|
+    puts "An object is saved successfully !"
+  end
+
+  after_save :set_msge
+  def set_msge
+    puts "An object is saved successfully !"
+  end
 end
