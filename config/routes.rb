47e417a7578b1600552d6e87bec2c83f22941b95
Abhishek Badmaliya <abhishek.badmaliya@simformsolutions.com>
2022-03-24 17:44:39 +0530

    feat: TE5-T91, create employee model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+  get 'employees/create'
+  get 'employees/update'
+  get 'employees/destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
