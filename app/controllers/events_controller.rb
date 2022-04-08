92a8928543b0166875ad69c19aa2d1215ca90105
Abhishek Badmaliya <abhishek.badmaliya@simformsolutions.com>
2022-04-08 10:21:25 +0530

    feat: TE5-T93, add methods on event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,30 +3,54 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = "Event Added Successfully!"
+      redirect_to events_path
+    else
+      flash[:error] = "Event can't be added!"
+      render :new
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
     if @event.update(event_params)
-      flash[:notice] = "Event details updated successfully."
-      redirect_to @event
+      flash[:notice] = "Event's records Updated Successfully !"
+      redirect_to events_path
     else
-      render 'edit'
+      flash[:notice] = "Oops, Updation Operation Failed !"
+      render :edit
     end
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      flash[:notice] = "Event's records Deleted Successfully !"
+      redirect_to events_path
+    else
+      flash[:notice] = "Oops, Deletion Operation Failed !"
+    end
   end
 
   private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
     def event_params
       params.require(:event).permit(:name, :description, :event_date)
     end
